3a9d25a657daa853c11aa9c81be60d0b589639f9
Chenzhi Ma <machenzhi123@gmail.com>
2023-04-15 18:08:56 +0800

    train updata

--- src/train.py
+++ src/train.py
@@ -9,12 +9,14 @@ from stable_baselines3.common.env_checker import check_env
 env = HighwayEnv()
 # check_env(env)
 
-# model = DQN("MlpPolicy", env, verbose=1)
-# model.learn(total_timesteps=200000)
+model = DQN("MlpPolicy", env, verbose=1)
+
+model.learn(total_timesteps=2000)
 # model.save("model")
-# print(f"Model saved")
+print(f"Model saved")
+
 # del model
-model = DQN.load("model", env=env)
+# model = DQN.load("log/best_model.zip", env=env)
 
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=40)
 print(f"Mean reward = {mean_reward}, std_reward = {std_reward}")
